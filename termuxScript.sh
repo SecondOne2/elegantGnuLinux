f12a8e946eb9a50a72202fa417a94441c25e736f
axelanimus <pptoroino@gmail.com>
2022-08-27 21:31:54 -0500

    Let's to test this shit

--- termuxScript.sh
+++ termuxScript.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!~/../usr/bin/env bash
 
 # Script made by TitanHero https://github.com/TitanHero
 echo -e "\e[1;3;32;40mEasyTermux\n\e[0;1;4;36mThis Script will install useful stuff to can manage \033[0;1;5;34mTermux\033[0;1;4;36m in a most easy way\e[0m\nTake care of \e[0;1;32mplants\033[0m\n"
 
 # Let's to put a few of vars with the binaries to install
 
-declare -a necessary=(tmux wget file tar zip dnsutils mlocate nmap python golang man littlecms git binutils proot radare2 nano dropbear cronie bash-completion pip eg tldr ipython)
+declare -a necessary=(python tmux wget file tar zip dnsutils mlocate nmap python golang man littlecms git binutils proot radare2 nano dropbear cronie bash-completion pip33 eg tldr ipython)
 
 options=("List_binaries" "Full_install" "Help" "Exit")
 
@@ -24,7 +24,7 @@ select opt in ${options[*]}; do
         yesNo="Yes No"
         select topt in $yesNo; do
             if [[ "$topt" = "Yes" ]]; then
-                pkg install ${necessary[*]:0:20}; pip install --upgrade ${necessary[*]:20:4};cp bashrc $HOME/.bashrc;ln -s /storage/emulated/0 $HOME/storage; go get -u github.com/jingweno/ccat; grep ccat $HOME/.bashrc; [[ $? -ne 0  ]] && echo -e  "PATH+=\":$HOME/go/bin\"\nalias cat='ccat'" >> ~/.bashrc; wget https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS; mv LS_COLORS $HOME/.LS_COLORS; grep LS_COLORS ~/.bashrc; [[ $? -ne 0 ]] && echo "eval \$( dircolors -b ~/.LS_COLORS )" >> $HOME/.bashrc; source ~/.bashrc ; printf "\n\n Ok ready if you want to exit of this script select the exit option, animus, \n\033[1mPeace and Love\e[0m\n\n" ;main
+                pkg install ${necessary[*]:0:21}; pip3 install --upgrade ${necessary[*]:21:4};cp bashrc $HOME/.bashrc; go get -u github.com/jingweno/ccat; grep ccat $HOME/.bashrc; [[ $? -ne 0  ]] && echo -e  "PATH+=\":$HOME/go/bin\"\nalias cat='ccat'" >> ~/.bashrc; wget https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS; mv LS_COLORS $HOME/.LS_COLORS; grep LS_COLORS ~/.bashrc; [[ $? -ne 0 ]] && echo "eval \$( dircolors -b ~/.LS_COLORS )" >> $HOME/.bashrc; source ~/.bashrc ; printf "\n\n Ok ready if you want to exit of this script select the exit option, animus, \n\033[1mPeace and Love\e[0m\n\n" ;main
             elif [[ $topt = "No" ]]; then
             echo -e "\nOk maybe you aren't ready for this, maybe in the future, anyway ...Animus\nNeverGiveUp\nKeepTrying\nPeaceAndLove\nSaveThePlanet\n"; main
             else
